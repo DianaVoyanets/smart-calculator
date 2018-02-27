00604b5afd865fd2b6082c51308a794a3812edb0
d.voyanets <diana.voyanets@gmail.com>
2018-02-28 00:32:04 +0300

    Done!

--- src/index.js
+++ src/index.js
@@ -1,27 +1,42 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.initialValue = initialValue.toString();  
   }
 
   add(number) {
-    // your implementation
+    this.initialValue += '+' + number;
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.initialValue += '-' + number;
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.initialValue += '*' + number;
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.initialValue += '/' + number;
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.initialValue += '**' + number;
+
+    return this;
   }
+  
+  toString() {
+    return this.valueOf(); 
+  }
+
+  valueOf() {
+    return eval(this.initialValue);
+  }
+
 }
 
 module.exports = SmartCalculator;
